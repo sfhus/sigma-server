89cf1ce66232f7586ab015ada4aaaa14d47adb58
Faraz <15679360+farazsfh@users.noreply.github.com>
2020-07-08 13:41:09 -0400

    removed secret

--- server_files/Credentials.js
+++ server_files/Credentials.js
@@ -3,8 +3,8 @@ var SpotifyWebApi = require('spotify-web-api-node');
 // Store application details, must match details on spotify dashboard
 const credentials = {
     redirectUri: 'http://localhost:3000/playlists/',
-    clientId: '23255368548e4c4694a5cee85b5e59a1',
-    clientSecret: 'c0e3fe4df9fd4c5397816f2490efee2a'
+    clientId: '',
+    clientSecret: ''
 }
 
 var spotifyApi = new SpotifyWebApi(credentials);
